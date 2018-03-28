c606b433192c2ee891296cda7a9c26025c45b4e9
Nathan Hunt <neighthan.hunt@gmail.com>
2018-03-27 23:30:05 -0400

    Adding memories works!
    A ListView.Builder is used instead of just the ListView so that when new MemoryWidgets are added, the display updates accordingly.
    The Memory class was renamed to MemoryWidget and a new Memory class (which isn't a widget) was created. Decoupling this makes it so we can change the attributes of a Memory, which we do while constructing it in AddMemory (see below). Once a Memory is finished, it can be wrapped with MemoryWidget to add on the display information.
    The AddMemory page creates an empty Memory (if editing a memory, the values from the memory being edited are used instead) which is updated whenever the TextFields (date, tags, text) are updated. When Save is pressed, a MemoryWidget is created from this Memory and added to the list.
    MemoryListState now defines methods for adding new memories and updating (i.e. overwriting) existing ones. Defining these methods in a State lets us call setState whenever the list of memories is updated so that the UI will be refreshed.

--- lib/main.dart
+++ lib/main.dart
@@ -13,67 +13,110 @@ class MemoryApp extends StatelessWidget {
 }
 
 class MemoryList extends StatefulWidget {
-  final List<Memory> _memories = [['1-1', 'R, s', 'Memory 1'], ['1-2', 'R, s', 'Memory 2']]
-  .map((mem) {
-    return new Memory(date: mem[0], tags: mem[1], text: mem[2]);
-  }).toList();
   @override
   MemoryListState createState() => new MemoryListState();
 }
 
 class MemoryListState extends State<MemoryList> {
+  final List<MemoryWidget> _memories = [['1-1', 'R, s', 'Memory 1'], ['1-2', 'R, s', 'Memory 2']]
+  .map((mem) {
+    return new MemoryWidget(new Memory(mem[0], mem[1], mem[2]));
+  }).toList();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Memory'),
       ),
-      body: new ListView(
-        children: widget._memories,
+      body: new ListView.builder(
+        itemCount: _memories.length,
+        itemBuilder: (BuildContext context, int idx) {
+          return _memories[idx];
+        },
       ),
       floatingActionButton: new FloatingActionButton(
         tooltip: 'Add Memory',
         child: new Icon(Icons.add),
-        onPressed: _addMemory,
+        onPressed: _addMemoryRoute,
       ),
     );
   }
 
-  _addMemory() {
+  _addMemoryRoute() {
     Navigator.of(context).push(
       new MaterialPageRoute(
         builder: (context) {
-          return new AddEditMemory('Add');
+          return new AddEditMemory('Add', this);
         }
       )
     );
   }
+
+  _addMemory(Memory memory) {
+    setState(() {
+      _memories.add(new MemoryWidget(memory));
+    });
+  }
+
+  _updateMemory(Memory memory, int memoryIdx) {
+    setState(() {
+      _memories[memoryIdx] = new MemoryWidget(memory);
+    });
+  }
 }
 
-class Memory extends StatelessWidget {
-  Memory({this.date, this.tags, this.text});
-  final date;
-  final tags;
-  final text;
+class Memory {
+  Memory(this.date, this.tags, this.text);
+  String date;
+  String tags;
+  String text;
+}
+
+class MemoryWidget extends StatelessWidget {
+  MemoryWidget(this.memory);
+  final Memory memory;
 
   @override
   Widget build(BuildContext context) {
     return new Row(children: <Widget>[
       new Column(children: <Widget>[
-        new Text(date),
-        new Text(tags),
+        new Text(memory.date),
+        new Text(memory.tags),
       ],),
-      new Text(text),
+      new Text(memory.text),
     ]);
   }
 }
 
 class AddEditMemory extends StatelessWidget {
-  AddEditMemory(this.addOrEdit);
+  AddEditMemory(this.addOrEdit, this.memoryListState, [this.memoryIdx]);
   final String addOrEdit;
+  final MemoryListState memoryListState;
+  final int memoryIdx;
 
   @override
   Widget build(BuildContext context) {
+    final Memory memory = new Memory('', '', '');
+
+    _addMemory() {
+      if (addOrEdit == 'Add') {
+        memoryListState._addMemory(memory);
+      } else {
+        assert(addOrEdit == 'Edit');
+        assert(memoryIdx != null);
+        memoryListState._updateMemory(memory, memoryIdx);
+      }
+
+      Navigator.of(context).pop();
+    }
+
+    if (memoryIdx != null) {
+      memory.text = memoryListState._memories[memoryIdx].memory.text;
+      memory.tags = memoryListState._memories[memoryIdx].memory.tags;
+      memory.date = memoryListState._memories[memoryIdx].memory.date;
+    }
+
     return new Scaffold(
       appBar: new AppBar(title: new Text('$addOrEdit Memory')),
       body: new Column(children: <Widget>[
@@ -82,28 +125,32 @@ class AddEditMemory extends StatelessWidget {
             child: new Text('Date'),
             padding: new EdgeInsets.all(8.0),
           ),
-          new Expanded(child: new TextField()),
+          new Expanded(child: new TextField(
+            onChanged: (text) => memory.date = text,
+          )),
         ]),
         new Row(children: <Widget>[
           new Container(
             child: new Text('Tags'),
             padding: new EdgeInsets.all(8.0),
           ),
-          new Expanded(child: new TextField()),
+          new Expanded(child: new TextField(
+            onChanged: (text) => memory.tags = text,
+          )),
         ]),
         new Container(
           child: new Text('Memory'),
           padding: new EdgeInsets.all(16.0),
         ),
         new Expanded(child: new Container(
-          child: new TextField(maxLines: null),
+          child: new TextField(
+            maxLines: null,
+            onChanged: (text) => memory.text = text,
+          ),
           padding: new EdgeInsets.all(8.0),
         )),
         new RaisedButton(onPressed: _addMemory, child: new Center(child: new Text('Save')))
       ]),
     );
-  }
-
-  _addMemory() {
   }
 }
